fbdeb6ceb8a5d990f796195bc3fc07b23efbdab8
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2020-03-02 19:27:40 -0600

    Updates

--- precache-manifest.3d0ef4789d96ff3976a7d508490b5cb1.js
+++ precache-manifest.3d0ef4789d96ff3976a7d508490b5cb1.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f8c0e00a3dd0f7dd3a1ff69d3d126b52",
+    "revision": "4203c469630c43d81fd198e4c982985b",
     "url": "/index.html"
   },
   {
-    "revision": "2191531439cb701422d7",
+    "revision": "68759d665fffdef91df9",
     "url": "/static/css/2.b0bb7f2c.chunk.css"
   },
   {
-    "revision": "6aea7ea3d621edce1546",
-    "url": "/static/css/main.b32aa38d.chunk.css"
+    "revision": "135df8515313b7e3b9db",
+    "url": "/static/css/main.61501159.chunk.css"
   },
   {
-    "revision": "2191531439cb701422d7",
-    "url": "/static/js/2.8a1ef5c0.chunk.js"
+    "revision": "68759d665fffdef91df9",
+    "url": "/static/js/2.da8b838b.chunk.js"
   },
   {
-    "revision": "6aea7ea3d621edce1546",
-    "url": "/static/js/main.27d7e225.chunk.js"
+    "revision": "135df8515313b7e3b9db",
+    "url": "/static/js/main.e2f40acb.chunk.js"
   },
   {
     "revision": "57b29f19cdbd9be10582",
@@ -26,6 +26,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "421908e67c836cdbb1c92fd0546bd7b3",
     "url": "/static/media/Arrow-down.421908e6.svg"
+  },
+  {
+    "revision": "07883e93734b98cae0f7b9c55d287250",
+    "url": "/static/media/GitHub_Logo.07883e93.svg"
   },
   {
     "revision": "64b2039716fedcc3398183360ae468fd",
